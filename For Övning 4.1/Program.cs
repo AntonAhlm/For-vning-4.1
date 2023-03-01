9e1d9108287ec3ed931a793c418574b904cc074a
Anton <>
2023-03-01 10:02:50 +0100

    Add project files.

---
+++ For Övning 4.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 40; i <= 80; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
